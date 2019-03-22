886c0fc6a0ab2f70d33748bbe3cc161b0ccce131
SatishR <sramah@gmail.com>
2019-03-22 14:27:28 -0700

    This is my first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FridayTestRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
